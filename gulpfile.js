740f75f1c3aae3487ab633d267ff1d864e9f0f99
Aurélien Roux <aureroux13@me.com>
2017-10-17 13:13:33 -0400

    init gulp and npm

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+'use strict';
+
+//Dependencies required
+var gulp = require('gulp');
+
+//Sass
+var sass = require('gulp-sass');
+var sourceMap = require('gulp-sourcemaps');
+//MISC
+var browserSync = require('browser-sync').create();
+var del = require('del');
+var plumber = require('gulp-plumber');
+
+
+// compile Sass files
+gulp.task('sass', function(){
+  gulp.src('src/styles/main.scss')
+  .pipe(sourceMap.init())
+    .pipe(plumber())
+    .pipe(sass())
+  .pipe(sourceMap.write())
+  .pipe(gulp.dest('dist/styles/'))
+})
+
+gulp.task('clean', function() {
+  del(['dist/styles/**.*']);
+})
+
+// Gulp defaut tasks
+gulp.task('default', ['sass'], function(){
+  console.log("Default gulp task *-*-*-*")
+  gulp.watch('src/stylesSass/**/*.scss', ['sass']);
+});
